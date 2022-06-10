3b8633a01a81973625b0f4986fd63a0c8df93c0f
Miguel Gonzalez <miguel.gonzalez6@vodafone.com>
2022-06-10 12:07:45 +0100

    Add worker

--- 06_multi/worker.js
+++ 06_multi/worker.js
@@ -3,7 +3,11 @@ if (isMainThread) {
     // This code is executed in the main thread and not in the worker.
     const worker = new Worker(__filename);
     worker.on('message', (msg) => { console.log(msg); });
+    setTimeout(() => {
+        worker.postMessage('Hello child')
+    }, 1000);
 
 } else {
-    parentPort.postMessage('Hello world!');
+    parentPort.postMessage('Hi dad');
+    parentPort.on('message', message => console.log('Dad says: ' + message));
 }
